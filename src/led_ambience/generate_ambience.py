24716b4afbb629ceeb8be7ff69eccd321ec920a4
AydenClay96 <AydenClay1996@gmail.com>
2024-12-31 14:30:17 +0000

    grunt: added gitignore file and deleted some files that shouldn't be on Git.

--- src/led_ambience/generate_ambience.py
+++ src/led_ambience/generate_ambience.py
@@ -4,10 +4,10 @@ from generate_strip import LEDStrip
 
 
 def main() -> None:
-    screen_size = [60, 45]
-    screen_resolution = [30, 30]
-    n_led = 1000
-    neighbours = 50
+    screen_size = [16, 10]
+    screen_resolution = [50, 50]
+    n_led = 300
+    neighbours = 15
 
     screen = Screen(size=screen_size, resolution=screen_resolution)
     strip = LEDStrip(number=n_led, size=screen_size)
@@ -16,9 +16,9 @@ def main() -> None:
     screen.plot()
     strip.plot()
 
-    ax = plt.gca()
-    ax.set_xlim((-10, screen_size[0] + 10))
-    ax.set_ylim((-10, screen_size[1] + 10))
+    # ax = plt.gca()
+    # ax.set_xlim((-10, screen_size[0] + 10))
+    # ax.set_ylim((-10, screen_size[1] + 10))
     plt.show()
 
 
